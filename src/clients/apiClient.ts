607e6952084c4d26890bab539e88628e0f006688
smithamaris <smith.amaris@gmail.com>
2025-12-13 00:23:17 -0500

    .env

--- src/clients/apiClient.ts
+++ src/clients/apiClient.ts
@@ -11,3 +11,10 @@ apiClient.interceptors.request.use((config) => {
   }
   return config;
 });
+
+// export const apiClient = axios.create({
+//     baseURL: import.meta.env.VITE_BACKEND_URL,
+//     headers: {
+//         Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzcyMDhkNWJjZGY0MjE2YzVkMGViMCIsInVzZXJuYW1lIjoiTW9hbmEiLCJlbWFpbCI6InRlc3QxMUB0ZXN0LmNvbSIsInJvbGUiOiJ1c2VyIn0sImlhdCI6MTc2NTU4ODkzMiwiZXhwIjoxNzY1Njc1MzMyfQ.YTGEXCeZNWU3oEI31H9GuFxz6UcvPlLleyOmkVDRNKU"
+//     }
+// });
